403998fe3b1dd4e825174972da11a72fb511271f
Atul Akella <44159259+atulakella@users.noreply.github.com>
2024-09-12 20:56:43 +0530

    Update data_fetching.py

--- data_fetching.py
+++ data_fetching.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # Configure the API with your actual API key
-genai.configure(api_key="AIzaSyCjvhclrgeAWAQVMRDQ2O3Bti6i2y_YoDQ")
+genai.configure(api_key="<Your-api-key>")
 
 # Initialize the model
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
